3a36b2dc4f5938ef1e6822e31c28f757d076723e
fahmirumagutawan <fahmigutawan@gmail.com>
2024-06-15 22:46:33 +0700

    added UI home

--- app/src/main/java/com/example/melali/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/melali/presentation/home/HomeScreen.kt
@@ -2,12 +2,21 @@ package com.example.melali.presentation.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,19 +25,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.melali.components.RecommendationItem
 
 @Composable
 fun HomeScreen(navController: NavController) {
     val viewModel = hiltViewModel<HomeViewModel>()
 
     Column(
-        modifier = Modifier.padding(horizontal = 24.dp),
         verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {
         Spacer(modifier = Modifier)
 
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            OutlinedIconButton(
+                onClick = { /*TODO*/ },
+                shape = CircleShape
+            ) {
+                Icon(imageVector = Icons.Default.Person, contentDescription = "")
+            }
+        }
+
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
             value = viewModel.searchInput.value,
             onValueChange = {
                 viewModel.searchInput.value = it
@@ -40,6 +65,40 @@ fun HomeScreen(navController: NavController) {
                 Icon(imageVector = Icons.Default.Search, contentDescription = "")
             }
         )
+
+        Column (
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ){
+            Text(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "Rekomendasi untuk Anda"
+            )
+
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                item {
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+
+                items(10) {
+                    RecommendationItem(url = "", name = "Pantai", location = "Ubud, bali")
+                }
+
+                item {
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
+        }
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "Buat Jadwal Baru")
+        }
 
         Spacer(modifier = Modifier)
     }
