ff70cad94973f9cd8a9b70f6a2180fda0f07c0be
mayunngrh <130374006+mayunngrh@users.noreply.github.com>
2024-06-19 02:19:10 +0700

    add some category, price, and disability friendly tag

--- app/src/main/java/com/example/melali/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/example/melali/presentation/detail/DetailScreen.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -20,7 +21,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -36,6 +39,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -148,13 +152,143 @@ fun DetailScreen(
                 )
             }
         }
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
+            ) {
+                Box {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(MaterialTheme.colorScheme.secondaryContainer)
+                            .height(78.dp)
+                            .width(78.dp)
+                            .align(Alignment.Center)
+
+                    ) {
+                        AsyncImage(
+                            model = R.drawable.category,
+                            contentDescription = "",
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondaryContainer),
+                            modifier = Modifier.padding(18.dp)
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Category",
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = "${destinasi?.category}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
+            ) {
+                Box {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(MaterialTheme.colorScheme.secondaryContainer)
+                            .height(78.dp)
+                            .width(78.dp)
+                            .align(Alignment.Center)
+
+                    ) {
+                        AsyncImage(
+                            model = R.drawable.disability_friendly,
+                            contentDescription = "",
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondaryContainer),
+                            modifier = Modifier.padding(18.dp)
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    text = "Disability Friendly",
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = if (destinasi?.Is_accessibility == true) "Yes" else "No",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
+            ) {
+                Box {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(MaterialTheme.colorScheme.secondaryContainer)
+                            .height(78.dp)
+                            .width(78.dp)
+                            .align(Alignment.Center)
+
+                    ) {
+                        AsyncImage(
+                            model = R.drawable.ticket,
+                            contentDescription = "",
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondaryContainer),
+                            modifier = Modifier.padding(18.dp)
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Ticket",
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = "${destinasi?.price.toString().take(2)} K",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+
+        Spacer(Modifier.height(32.dp))
+        Divider(
+            color = MaterialTheme.colorScheme.onSecondaryContainer,
+            thickness = 1.dp,
+            modifier = Modifier.padding(horizontal = 32.dp)
+        )
+        Spacer(Modifier.height(16.dp))
+
         Row(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(start = 32.dp, end = 24.dp, bottom = 6.dp, top = 32.dp),
+                .background(MaterialTheme.colorScheme.secondaryContainer.copy(alpha = 0.4f))
+                .padding(start = 32.dp, end = 24.dp),
             verticalAlignment = Alignment.CenterVertically,
 
-        ) {
+
+            ) {
             Text(
                 text = "${destinasi?.address}",
                 maxLines = 2,
@@ -165,15 +299,23 @@ fun DetailScreen(
             )
 
             Spacer(modifier = Modifier.width(12.dp))
-            Button(onClick = {
-                val mapIntentUri = Uri.parse("${destinasi?.url}")
-                val mapIntent = Intent(Intent.ACTION_VIEW, mapIntentUri)
-                context.startActivity(mapIntent)
-            }, contentPadding = PaddingValues(horizontal = 12.dp)) {
-                Text(text = "Buka Map", style= MaterialTheme.typography.labelLarge)
+            Button(
+                onClick = {
+                    val mapIntentUri = Uri.parse("${destinasi?.url}")
+                    val mapIntent = Intent(Intent.ACTION_VIEW, mapIntentUri)
+                    context.startActivity(mapIntent)
+                }, contentPadding = PaddingValues(horizontal = 12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.onSecondaryContainer
+
+                )
+            ) {
+                Text(text = "Buka Map", style = MaterialTheme.typography.labelLarge)
             }
         }
+        Spacer(modifier = Modifier.height(6.dp))
         Column(modifier = Modifier.padding(start = 32.dp, end = 32.dp)) {
+            Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = "About",
                 style = MaterialTheme.typography.titleLarge,
@@ -194,25 +336,17 @@ fun DetailScreen(
                 })
 
         }
-
-
-        Row {
-            Column {
-                Box {
-                    Box(
-                        modifier = Modifier
-                            .background(MaterialTheme.colorScheme.secondaryContainer)
-                            .height(48.dp)
-                            .width(48.dp)
-                    )
-                }
-            }
-        }
+        Spacer(modifier = Modifier.height(72.dp))
 
     }
-
-
 }
 
+@Preview
+@Composable
+fun miawmiaw() {
+    Box(Modifier.background(Color.White)) {
+        DetailScreen(navController = rememberNavController(), index = 1)
+    }
+}
 
 
